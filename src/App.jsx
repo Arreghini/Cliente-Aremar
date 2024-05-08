31d9f6cded8fe6380a5ce15effb7ff58d619913e
Arreghini <marreghi@gmail.com>
2024-05-08 20:59:44 -0300

    8/5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-export default function App() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import DetailView from "./views/DetailView";
+import HomeView from "./views/HomeView";
+import LandingView from "./views/LandingView";
+import NavBar from "./components/navbar/Navbar";
+
+function MainLayout() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingView />} />
+        <Route path="/home" element={<HomeView />} />
+        <Route path="/detail/:id" element={<DetailView />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function App() {
+  return <MainLayout />;
+}
+
+export default App;
+
+
+ 
