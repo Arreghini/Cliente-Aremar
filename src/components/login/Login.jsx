f809ded4bdaddbeb1546a42a487de6404820a59e
Arreghini <marreghi@gmail.com>
2024-06-19 16:14:16 -0300

    ++loguin 19/6

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
               required
             />
           </div>
-          <div className="flex justif y-center mt-4">
+          <div className="flex justif y-center justify-center mt-4">
             <button
               type="submit"
               className="w-1/3 justify-center bg-yellow-500 text-white font-semibold text-xl py-2 px-4 rounded hover:bg-yellow-600"
