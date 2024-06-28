45f7ef34928fdd613893ce3f12d1c6363907021d
Arreghini <marreghi@gmail.com>
2024-06-28 20:11:42 -0300

    login 28/6

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation, BrowserRouter as Router } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import DetailView from './views/DetailView';
 import HomeView from './views/HomeView';
 import OffersView from './views/OffersView';
 import LandingView from './views/LandingView';
 import Navbar from './components/navbar/Navbar';
-import Profile from './components/profile/Profile'
+import Profile from './components/profile/Profile';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import LoginForm from './components/login/LoginForm';
 
 initializeIcons();
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
 function MainLayout() {
-  const location= useLocation();  
+  const location = useLocation();
   return (
     <>
       {location.pathname !== '/login' && <Navbar />}
@@ -29,7 +33,17 @@ function MainLayout() {
 }
 
 function App() {
-  return <MainLayout />;
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      
+        <MainLayout />
+      
+    </Auth0Provider>
+  );
 }
 
 export default App;
