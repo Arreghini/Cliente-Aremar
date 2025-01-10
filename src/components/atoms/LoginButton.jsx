ea0f1925fa97e2011a326f90873703592c3d66f7
Mauricio Arreghini <marreghi@gmail.com>
2025-01-10 00:24:32 -0300

    Guardando cambios locales antes de pull

--- src/components/atoms/LoginButton.jsx
+++ src/components/atoms/LoginButton.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
-return (
+  return (
     <div className="flex justify-center mt-4">
-<button onClick={() => loginWithRedirect()}
-  className=" justify-center bg-yellow-400 text-white font-semibold text-xl py-2 px-4 rounded hover:bg-yellow-600"
->
-  LOGIN
-</button>
+      <button
+        onClick={() =>
+          loginWithRedirect({
+            prompt: 'select_account', // Fuerza el selector de cuentas
+          })
+        }
+        className="justify-center bg-yellow-400 text-white font-semibold text-xl py-2 px-4 rounded hover:bg-yellow-600"
+      >
+        LOGIN
+      </button>
     </div>
-    )
-}
+  );
+};
 
 export default LoginButton;
--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,8 @@ export default {
       },
     },
     plugins: [],
+    server: {
+      port: 5173,
+    },
   };
   
